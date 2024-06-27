9c550b18d21b390acefb04ed8c1435e55aa167c4
Abhijit Paul <abhpaul@gmail.com>
2024-06-27 12:03:12 +0200

    first commit

--- TypeScript/index.test.ts
+++ TypeScript/index.test.ts
@@ -1,5 +1,37 @@
-import { placeholder } from "./index";
+import { playerScore, score } from "./index";
 
-it("works", () => {
-  expect(placeholder()).toBe(2);
+
+
+it("1st Score point : ", () => {
+  let player: playerScore = {name: "TestPlayer", score: 0};
+  score(player);
+  expect(player.score).toBe(15);
+});
+
+it("2nd Score point : ", () => {
+  let player: playerScore = {name: "TestPlayer", score: 15};
+  score(player);
+  expect(player.score).toBe(30);
+}); 
+
+it("3rd Score point : ", () => {
+  let player: playerScore = {name: "TestPlayer", score: 30};
+  score(player);
+  expect(player.score).toBe(40);
+});
+
+/// 2nd level 
+it("1st Score point with 2 players : ", () => {
+  let player1: playerScore = {name: "Player A", score: 0};
+  let player2: playerScore = {name: "Player B", score: 15};
+  score(player2);
+  expect(player1.score).toBe(0);
+  expect(player2.score).toBe(30);
+});
+
+/// 3rd level 
+it("Set Win test : ", () => {
+  let player: playerScore = {name: "TestPlayer", score: 40, setOutcome: false};
+  score(player);
+  expect(player.setOutcome).toBe(true);
 });
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,6 +1,28 @@
-export function placeholder() {
-  console.log(1);
-  return 2;
+
+//enum scorePoint { "15", "30", "40"}
+
+let currentScore = 0;
+
+export interface playerScore {
+  name: String,
+  score: number,
+  setOutcome?: boolean 
 }
 
-placeholder();
+export function score(player: playerScore): playerScore {
+
+  if(player.score === 0) { 
+    player.score  = 15;
+  } else if (player.score === 15 ) { 
+    player.score  = 30;
+  } else if (player.score === 30 ) {
+    player.score  = 40;
+  } else if (player.score === 40) {
+    player.setOutcome = true;
+    //player.score  = 0;
+  }
+  return player;
+}
+
+export default score;
+
